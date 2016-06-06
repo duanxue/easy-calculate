66e2b2021699c1c8d52db60b2e63131961180ca3
duanxue <1360132676@qq.com>
2016-06-06 18:27:22 +0800

    first commit

---
+++ src/calculate-median.js
@@ -0,0 +1,29 @@
+function calculate_median(arr) {
+  //请勿改动函数名
+	var flag=true,temp=[],j=0;
+	for(var i=1;i<=arr.length;i++)
+	   {
+		if(i%2==0)
+		   temp[j++]=arr[i-1];
+	   }  
+	for(var k=temp.length-2;k>=0&&flag;k--)
+	   {
+		flag=false;
+		for(var m=0;m<=k;m++)
+		   {
+			if(temp[m]>temp[m+1])
+				{
+					var tempnumber=temp[m];
+					temp[m]=temp[m+1];
+					temp[m+1]=tempnumber;
+					flag=true;	
+				}
+		   }
+	   }
+	if(temp.length%2!=0)
+		return  temp[parseInt(temp.length/2)];
+	else
+                return  (temp[temp.length/2]+temp[temp.length/2-1])/2;
+}
+
+module.exports = calculate_median;
